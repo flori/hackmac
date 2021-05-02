4c64ef34fd8d2e2d0d3a9aacc0bb57b37f8ded3a
Florian Frank <flori@ping.de>
2021-05-02 14:23:42 +0200

    Support multiple config pathes via env var CONFIG_PATH

--- lib/hackmac/config.rb
+++ lib/hackmac/config.rb
@@ -1,6 +1,7 @@
 require 'complex_config/shortcuts'
 require 'tins/xt/secure_write'
 require 'fileutils'
+require 'pathname'
 
 module Hackmac
   module Config
@@ -46,15 +47,15 @@ module Hackmac
             github: 'acidanthera/WhateverGreen'
     end
 
-    def self.load
-      path = File.expand_path('~/.config/hackmac')
-      mkdir_p path
-      ComplexConfig::Provider.config_dir = path
-      config_path = File.join(path, 'hackmac.yml')
-      unless File.exist?(config_path)
-        File.secure_write(config_path, DEFAULT)
+    def self.load(path: ENV.fetch('CONFIG_PATH', '~/.config/hackmac/hackmac.yml'))
+      path = Pathname.new(path).expand_path
+      mkdir_p path.dirname
+      ComplexConfig::Provider.config_dir = path.dirname
+      unless path.exist?
+        File.secure_write(path, DEFAULT)
       end
-      complex_config.hackmac
+      puts "Loading config from #{path.to_s.inspect}."
+      ComplexConfig::Provider[path.basename.to_s.sub(/\..*?\z/, '')]
     end
   end
 end
